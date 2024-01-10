ac7f4d9649e1495c447491247854f22855ee123e
BesartAbazi <besart45@hotmail.com>
2024-01-10 14:36:13 +0100

    first version

---
+++ server/minionsRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const minionsRouter = express.Router();
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+} = require('./db');
+
+minionsRouter.get('/', (req, res, next) => {
+    const allMinions = getAllFromDatabase('minions');
+    if (allMinions)
+        res.status(200).send(getAllFromDatabase)
+    else
+        res.status(404).send('Error: System error.')
+});
+
+module.exports = minionsRouter;
