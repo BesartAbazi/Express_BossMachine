ff8814a0ce5ab73b5b2d9d4e0890dd860ccc9287
BesartAbazi <besart45@hotmail.com>
2024-01-15 17:43:36 +0100

    2024-01-15

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -1,21 +1,68 @@
 const express = require('express');
 const minionsRouter = express.Router();
-//const {
-//    createMeeting,
-//    getAllFromDatabase,
-//    getFromDatabaseById,
-//    addToDatabase,
-//    updateInstanceInDatabase,
-//    deleteFromDatabasebyId,
-//    deleteAllFromDatabase,
-//} = require('./db');
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+} = require('./db');
+
+minionsRouter.param('id', (req, res, next, id) => {
+    const minion = getFromDatabaseById('minions', id);
+    if (minion){
+        req.minion = minion;
+        next();
+    }
+    else{
+        res.status(404).send('Error: Unknown minion id.');
+    }
+});
 
 minionsRouter.get('/', (req, res, next) => {
-    const allMinions = ''//getAllFromDatabase('minions');
+    const allMinions = getAllFromDatabase('minions');
     if (allMinions)
         res.status(200).send(allMinions)
     else
         res.status(404).send('Error: System error.')
 });
+
+minionsRouter.get('/:id', (req, res, next) => {
+    res.status(200).send(req.minion);
+});
+
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    if (newMinion){
+        res.status(201).send(newMinion);
+    }
+    else{
+        res.status(500).send();
+    }
+});
+
+minionsRouter.put('/:id', (req, res, next) => {
+    const updatedMinion = updateInstanceInDatabase('minions', req.body);
+    console.log(req.minion, updatedMinion)
+    if (updatedMinion){
+        res.status(201).send(updatedMinion);
+    }
+    else{
+        res.status(500).send();
+    }
+});
+
+minionsRouter.delete('/:id', (req, res, next) => {
+    const deleteResult = deleteFromDatabasebyId('minions', req.params.id);
+    if (deleteResult){
+        res.status(204).send('OK');
+    }
+    else{
+        res.status(500).send();
+    }
+});
+
 
 module.exports = minionsRouter;
